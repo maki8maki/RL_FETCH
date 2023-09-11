c32a8a1aaeb8885becc2532e9ef47e33ee7aed75
y_yamaguchi <chrisrerei125815@gmail.com>
2023-09-11 21:50:29 +0900

    メモリ不足の対処

--- agents/DCAE.py
+++ agents/DCAE.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from agents.utils import size_after_conv, Reshape
+from agents.utils import size_after_conv, size_after_pooling, Reshape
 
 class DCAE(nn.Module):
     def __init__(self, image_size, hidden_dim) -> None:
@@ -9,20 +9,27 @@ class DCAE(nn.Module):
         after_height = img_height
         after_width = img_width
         ksize = 3
+        pooling_size = 2
         for _ in range(len(channels)-1):
             after_height = size_after_conv(after_height, ksize=ksize)
+            after_height = size_after_pooling(after_height, pooling_size)
             after_width = size_after_conv(after_width, ksize=ksize)
+            after_width = size_after_pooling(after_width, pooling_size)
         after_size = after_height * after_width * channels[-1]
         features = [after_size, 1000, 150, hidden_dim]
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=ksize),
             nn.ReLU(),
+            nn.MaxPool2d(pooling_size),
             nn.Conv2d(in_channels=channels[1], out_channels=channels[2], kernel_size=ksize),
             nn.ReLU(),
+            nn.MaxPool2d(pooling_size),
             nn.Conv2d(in_channels=channels[2], out_channels=channels[3], kernel_size=ksize),
             nn.ReLU(),
+            nn.MaxPool2d(pooling_size),
             nn.Conv2d(in_channels=channels[3], out_channels=channels[4], kernel_size=ksize),
             nn.ReLU(),
+            nn.MaxPool2d(pooling_size),
             Reshape((-1, features[0])),
             nn.Linear(in_features=features[0], out_features=features[1]),
             # nn.BatchNorm1d(num_features=features[1]),
@@ -47,12 +54,16 @@ class DCAE(nn.Module):
             Reshape((-1, channels[-1], after_height, after_width)),
             nn.ConvTranspose2d(in_channels=channels[4], out_channels=channels[3], kernel_size=ksize),
             nn.ReLU(),
+            nn.Upsample(scale_factor=pooling_size),
             nn.ConvTranspose2d(in_channels=channels[3], out_channels=channels[2], kernel_size=ksize),
             nn.ReLU(),
+            nn.Upsample(scale_factor=pooling_size),
             nn.ConvTranspose2d(in_channels=channels[2], out_channels=channels[1], kernel_size=ksize),
             nn.ReLU(),
+            nn.Upsample(scale_factor=pooling_size),
             nn.ConvTranspose2d(in_channels=channels[1], out_channels=channels[0], kernel_size=ksize),
-            nn.Sigmoid()
+            nn.Sigmoid(),
+            nn.Upsample(size=(img_height, img_width))
         )
     
     def forward(self, x, return_pred=False):
